63fa15f07ee27e5f6b9ebd800b983cdba18a7efe
jinbing <1520006273@qq.com>
2014-04-22 21:26:28 +0800

    2014_4_22版本1

--- 1/gen/com/example/R.java
+++ 1/gen/com/example/R.java
@@ -8,6 +8,20 @@
 package com.example;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_in=0x7f040000;
+        public static final int fade_out=0x7f040001;
+        public static final int head_in=0x7f040002;
+        public static final int head_out=0x7f040003;
+        public static final int hold=0x7f040004;
+        public static final int hold_long=0x7f040005;
+        public static final int push_bottom_in=0x7f040006;
+        public static final int push_bottom_out=0x7f040007;
+        public static final int push_top_in=0x7f040008;
+        public static final int push_top_in2=0x7f040009;
+        public static final int push_top_out=0x7f04000a;
+        public static final int push_top_out2=0x7f04000b;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,27 +31,35 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-    }
-    public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int cx_fa_app_icon=0x7f020000;
+        public static final int loading=0x7f020001;
+        public static final int title_bar=0x7f020002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int mainactivity=0x7f030000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
+        public static final int AnimBottom=0x7f070003;
+        /**  ####################以上内容全部来自搬运 ############################################ 
+         */
+        public static final int AnimFade=0x7f070002;
+        public static final int AnimHead=0x7f070006;
+        public static final int AnimTop=0x7f070004;
+        public static final int AnimTop2=0x7f070005;
+        public static final int Anim_style=0x7f070007;
+        /**  定义其中打开或者保持或者关闭的时候的效果 
+         */
+        public static final int Anim_style2=0x7f070008;
+        public static final int Anim_style_title=0x7f070009;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -59,10 +81,22 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int CustomWindowTitleBackground=0x7f07000a;
+        /** 模糊
+         */
+        public static final int HeadScale=0x7f07000e;
+        public static final int MyDialogStyle=0x7f07000b;
+        /** 模糊
+         */
+        public static final int MyDialogStyleBottom=0x7f07000c;
+        /** 模糊
+         */
+        public static final int MyDialogStyleTop=0x7f07000d;
+        public static final int MyProgressBar=0x7f07000f;
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,14 @@ birthday
 ========
 
 an android app for anroid
+
+### 2014年4月21日 ###
+新建一个项目，解决了git遇到的一些问题
+
+### 2014年4月22日版本1 ###
+1.strings配合anim中加入各种android模式
+
+2.加入了cx_faapp_icon.png的android图标
+
+3.创建测试版本
+
