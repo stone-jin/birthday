c13f69485f4ca16b8fb57c7140561cf787aabbc5
jinbing <1520006273@qq.com>
2014-04-23 14:03:56 +0800

    2014年4月23日版本2

--- 1/gen/com/edu/birthday/R.java
+++ 1/gen/com/edu/birthday/R.java
@@ -66,12 +66,18 @@ public final class R {
     }
     public static final class id {
         public static final int birth_listview=0x7f080000;
+        public static final int mainactivity_imageview=0x7f080001;
+        public static final int mainactivity_tabs_birth=0x7f080002;
+        public static final int mainactivity_tabs_center=0x7f080004;
+        public static final int mainactivity_tabs_more=0x7f080005;
+        public static final int mainactivity_tabs_sms=0x7f080003;
     }
     public static final class layout {
         public static final int birth=0x7f030000;
         public static final int birth_title=0x7f030001;
-        public static final int mainactivity=0x7f030002;
-        public static final int mainactivity_tabs=0x7f030003;
+        public static final int helloworld=0x7f030002;
+        public static final int mainactivity=0x7f030003;
+        public static final int mainactivity_tabs=0x7f030004;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
